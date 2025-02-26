057adf41c6b281e8c28891d391b1b751f7f0a55d
VINIT KUMAR PATEL <101181057+CoderVinit@users.noreply.github.com>
2025-02-26 10:59:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let count = 0;
+let button = document.getElementById("btn")
+let paragraph = document.getElementById('para')
+
+paragraph.textContent = count;
+
+button.addEventListener('click',()=>{
+	alert(count)
+	paragraph.textContent = ++count
+	
+})
